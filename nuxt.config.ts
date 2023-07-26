0a187b1fea5becb0806a27e1bd827b7504861639
kCat-fun <kcat.fun.stu@gmail.com>
2023-07-27 05:43:02 +0900

    assetsディレクトリ及びiconの追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  app: {
+    head: {
+      charset: 'utf-16',
+      viewport: 'width=device-width',
+      title: 'kCat\'s Portfolio',
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/icon.ico' }
+      ],
+      htmlAttrs: {
+        lang: 'ja',
+        prefix: 'og: https://ogp.me/ns#'
+      },
+    }
+  },
+});
