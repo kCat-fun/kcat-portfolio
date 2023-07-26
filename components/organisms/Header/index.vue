eae6c5466e7137b4d4a24e58ba09cde9ba4658ce
kCat-fun <kcat.fun.stu@gmail.com>
2023-07-27 03:32:57 +0900

    layout.vueをcomponentごとに分けて実装（仮）

---
+++ components/organisms/Header/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <nav>
+            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="/practice">Practice</NuxtLink>
+            <NuxtLink to="/practice/counter">Counter</NuxtLink>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+}
+</script>
+
+<style src="./index.css" scoped></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,8 @@
 <template>
   <div>
-    <nav>
-      <NuxtLink to="/">Home</NuxtLink>
-      <NuxtLink to="/practice">Practice</NuxtLink>
-      <NuxtLink to="/practice/counter">Counter</NuxtLink>
-    </nav>
-    <slot />
+    <OrganismsHeader></OrganismsHeader>
+    <main>
+      <slot />
+    </main>
   </div>
 </template>
-
-<style scoped>
-nav * {
-  margin: auto 10px;
-}
-
-nav {
-  width: 100%;
-  background: gray;
-}
-</style>
