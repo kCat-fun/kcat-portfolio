8e99650eed3c25b0ba458b3f33988e9a6b5cf812
kCat-fun <kcat.fun.stu@gmail.com>
2024-05-14 05:10:47 +0900

    エラーを調整

--- pages/Blog/index.vue
+++ pages/Blog/index.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="Blog">
         <PageTitle>Blog</PageTitle>
-        <div class="BlogBlox" v-for="blog in articles" >
-            <BlogBox :title="blog.title" :tags='blog.tags' :date="blog.date" :synopsis="blog.context" :imgPath="blog.imageurl" :url="blog.blogurl" altStr="ブログ画像" />
+        <div class="BlogBlox" v-for="blog in articles">
+            <BlogBox :title="blog.title" :tags='blog.tags' :date="blog.date" :synopsis="blog.context"
+                :imgPath="blog.imageurl" :url="blog.blogurl" altStr="ブログ画像" />
         </div>
     </div>
 </template>
