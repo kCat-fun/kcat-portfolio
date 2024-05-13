4d79d3d1b8ec25de7e4899e0a1ad77fbf0ee7e12
kCat-fun <kcat.fun.stu@gmail.com>
2024-05-14 06:04:58 +0900

    blogのoverflow部分をcssで省略するように変更

--- components/molecules/BlogBox/index.vue
+++ components/molecules/BlogBox/index.vue
@@ -7,7 +7,7 @@
             <div class="content">
                 <Date>{{ date }}</Date>
                 <div class="title">
-                    {{ titleStr }}
+                    <p>{{ titleStr }}</p>
                 </div>
                 <p class="synopsis">
                     {{ synopsisStr }}
@@ -28,7 +28,7 @@
             <div class="content">
                 <Date><span class="date">{{ date }}</span></Date>
                 <div class="title">
-                    {{ titleStr }}
+                    <p>{{ titleStr }}</p>
                 </div>
                 <p class="synopsis">
                     {{ synopsisStr }}
@@ -85,84 +85,11 @@ export default {
     mounted() {
         this.titleStr = this.title;
         this.synopsisStr = this.synopsis;
-        window.addEventListener('resize', this.textTrim);
-        setTimeout(() => {
-            this.textTrim()
-        }, 100);
+        // window.addEventListener('resize', this.textTrim);
+        // setTimeout(() => {
+        //     this.textTrim()
+        // }, 100);
     },
-    methods: {
-        textTrim() {
-            this.titleTrim();
-            this.synopsisTrim();
-        },
-        titleTrim() {
-            // 表示したい行数
-            var row = 1;
-
-            // 文末に追加したい文字
-            var clamp = '…';
-
-            //CSSプロパティを取得
-            const sample = document.getElementsByClassName("title");
-
-            var style = window.getComputedStyle(sample[0]);
-            var fontsize = parseFloat(style.fontSize); //font-sizeを取得
-            let width = parseFloat(style.width); //widthを取得
-            var letterSpacing = parseFloat(style.letterSpacing); //letter-spacingを取得
-
-            width = window.innerWidth < 800 ? 300 * 0.95 : width;
-
-            // 収まる文字数を計算
-            var wordCount = Math.floor(width / fontsize) * row;
-
-            // letter-spacingも含めて計算する場合
-            // var wordCount = Math.floor(width / (fontsize + letterSpacing)) * row;
-
-            // 文字数を超えたら
-            if (this.titleStr.length > wordCount) {
-                const str = this.title.substr(0, (wordCount - 1)); //1文字削る
-                this.titleStr = str + clamp; //文末にテキストを足す
-            }
-            else {
-                const str = this.title.substr(0, (wordCount - 1)); //1文字削る
-                this.titleStr = str + (this.titleStr.length >= this.title.length ? '' : clamp);
-            }
-        },
-        synopsisTrim() {
-            // 表示したい行数
-            var row = 3;
-
-            // 文末に追加したい文字
-            var clamp = '...';
-
-            //CSSプロパティを取得
-            const sample = document.getElementsByClassName("synopsis");
-            var style = window.getComputedStyle(sample[0]);
-            var fontsize = parseFloat(style.fontSize); //font-sizeを取得
-            let width = parseFloat(style.width); //widthを取得
-            var letterSpacing = parseFloat(style.letterSpacing); //letter-spacingを取得
-
-            width = window.innerWidth < 800 ? 300 * 0.95 : width;
-
-            // 収まる文字数を計算
-            var wordCount = Math.floor(width / fontsize) * row;
-
-
-
-            // letter-spacingも含めて計算する場合
-            // var wordCount = Math.floor(width / (fontsize + letterSpacing)) * row;
-
-            // 文字数を超えたら
-            if (this.synopsis.length > wordCount) {
-                const str = this.synopsis.substr(0, (wordCount - 2)); //1文字削る
-                this.synopsisStr = str + clamp; //文末にテキストを足す
-            }
-            else {
-                const str = this.synopsis.substr(0, (wordCount - 2)); //1文字削る
-                this.synopsisStr = str + (this.synopsisStr.length >= this.synopsis.length ? '' : clamp);
-            }
-        },
-    }
 }
 </script>
 
