94c68a204a3b57d9af936e38e2bf1ca708783292
kCat-fun <kcat.fun.stu@gmail.com>
2024-05-16 20:18:47 +0900

    Activityでボタンでページ遷移

--- pages/Activity/index.vue
+++ pages/Activity/index.vue
@@ -2,7 +2,8 @@
     <div>
         <PageTitle>Activity</PageTitle>
         <div>
-            <nuxt-link v-for="activity in activities.contents" :to="'/Activity/Content?id=' + activity.id" class="work-box-link">
+            <nuxt-link v-for="activity in activities.contents" :to="'/Activity/Content?id=' + activity.id"
+                class="work-box-link">
                 <div>
                     <WorkBox class="WorkBox" :tag="activity.tag" :data="activity.data" :imgPath="activity.image.url"
                         altStr="サムネイル">
@@ -10,9 +11,14 @@
                     </WorkBox>
                 </div>
             </nuxt-link>
-            <WorkBoxDammy class="WorkBox" />
-            <WorkBoxDammy class="WorkBox" />
-            <WorkBoxDammy class="WorkBox" />
+            <WorkBoxDammy class="WorkBox dammy" />
+            <WorkBoxDammy class="WorkBox dammy" />
+            <WorkBoxDammy class="WorkBox dammy" />
+        </div>
+        <div class="button-area">
+            <button @click="getCMS(Math.max(0, --pageNum))" :class="pageNum > 0 ? '' : 'fade'"><</button>
+            <span>{{ pageNum+1 }}/{{ Math.floor(totalCount / limit)+1 }}</span>
+            <button @click="getCMS(Math.min(++pageNum, Math.floor(totalCount / limit)))" :class="pageNum < Math.floor(totalCount / limit) ? '' : 'fade'">></button>
         </div>
     </div>
 </template>
@@ -24,20 +30,31 @@ export default {
         return {
             ctx: useRuntimeConfig().public,
             activities: {},
+            pageNum: 0,
+            totalCount: 0,
+            limit: 8,
         }
     },
     mounted() {
         // console.log(this.ctx);
-        fetch(this.ctx.baseURL, {
-            headers: {
-                "X-MICROCMS-API-KEY": this.ctx.apiKey
-            }
-        }).then(res => res.json())
-            .then(json => {
-                console.log(json);
-                this.activities = json;
-            });
+        this.getCMS(this.pageNum);
     },
+    methods: {
+        getCMS(offset) {
+            this.pageNum = offset;
+            fetch((this.ctx.baseURL + "?limit=" + this.limit + "&offset=" + (offset * this.limit)), {
+                headers: {
+                    "X-MICROCMS-API-KEY": this.ctx.apiKey,
+                },
+            }).then(res => res.json())
+                .then(json => {
+                    // console.log(json);
+                    // console.log(json.totalCount);
+                    this.activities = json;
+                    this.totalCount = json.totalCount;
+                });
+        }
+    }
 }
 </script>
 
