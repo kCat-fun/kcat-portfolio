bfa36f89a4b000ca99747e12daaf0c172bf2af32
kCat <118804479+kCat-fun@users.noreply.github.com>
2025-04-05 17:56:02 +0900

    index.vue の更新

--- components/organisms/TopIntro/index.vue
+++ components/organisms/TopIntro/index.vue
@@ -2,7 +2,7 @@
     <div>
         <p>- About kCat -</p>
         <p class="intro">
-            公立はこだて未来大学に所属する学部３年の<b>kCat</b>です。<br />
+            公立はこだて未来大学に所属する学部４年の<b>kCat</b>です。<br />
             電子工作・ロボット制御、Webサイト制作、Pythonによる自動化、動画編集などなど様々なことを浅く広く勉強しています。<br />
             ハードウェアサークルをはじめ、様々なサークルで活動をしています。<br />
             <NuxtLink to="/About" class="link">> 詳細</NuxtLink>
